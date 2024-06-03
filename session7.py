c627cffed8ad33126fe3ce1cf4862f7e0ec0ed79
alinazaqat <alinazaqatali8@gmail.com>
2024-06-03 19:01:58 +1000

    Create session7.py
    first change commited

---
+++ session7.py
@@ -0,0 +1,5 @@
+# Topic: The for Loop (Count-Controlled)
+continents = ["asia", "africa", "north america", "south america", "europe", "antarctica", "australia"]
+imfroamconts = ["asia"]
+for continent in continents:
+  print(f"Im from {asia}!")
